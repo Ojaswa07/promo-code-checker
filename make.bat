687136fe6f589291d7993eab2a7142e2bbf784a1
Ojaswa07 <136168548+Ojaswa07@users.noreply.github.com>
2023-07-09 09:59:33 +0530

    Delete make.bat

--- make.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-REM Create the files
-echo Sample content for promotions.txt > promotions.txt
-echo Sample content for valid.txt > valid.txt
-echo Sample content for claimed.txt > claimed.txt
-
-echo Files created successfully.
